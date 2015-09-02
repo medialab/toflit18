fcb05fe0f29783da0f0f32886072113d5ce33dea
Yomguithereal <guillaumeplique@gmail.com>
2015-09-02 16:40:49 +0200

    Basic addressbar

--- client/js/components/app.jsx
+++ client/js/components/app.jsx
@@ -5,9 +5,20 @@
  * Root component for the application.
  */
 import React, {Component} from 'react';
+import Addressbar from 'react-addressbar';
 
-export default class App {
+export default class App extends Component {
+  onHashChange(url) {
+    const hash = location.hash;
+  }
+
   render() {
-    return <div id="main">Hello World!</div>;
+    return (
+      <div id="main">
+        <Addressbar onChange={this.onHashChange}
+                    value="/#/home" />
+        Hello World!
+      </div>
+    );
   }
 }
