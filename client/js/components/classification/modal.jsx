ee47b916704a083a4169c92c5ec9742157ffa0ea
Yomguithereal <guillaumeplique@gmail.com>
2015-11-02 12:41:13 +0100

    Modal title

--- client/js/components/classification/modal.jsx
+++ client/js/components/classification/modal.jsx
@@ -8,17 +8,54 @@ import React, {Component} from 'react';
 import {branch} from 'baobab-react/decorators';
 import {Row, Col} from '../bootstrap/grid.jsx';
 
+
+/**
+ * Main component.
+ */
+@branch({
+  cursors: {
+    target: ['states', 'classification', 'browser', 'current'],
+    modal: ['states', 'classification', 'modal']
+  }
+})
 export default class ClassificationModal extends Component {
   render() {
+    const {modal, target} = this.props;
+
     return (
       <div className="modal-wrapper">
         <Row className="full-height">
           <Col md={12} className="full-height">
             <div className="panel full-height">
-              This is a very fancy modal, isn't it?
+              <ModalTitle name={target.name} action={modal.type} />
             </div>
           </Col>
         </Row>
+      </div>
+    );
+  }
+}
+
+/**
+ * Displaying an helpful title.
+ */
+class ModalTitle extends Component {
+  render() {
+    const {name, action} = this.props;
+
+    let title;
+
+    if (action === 'update')
+      title = `Updating "${name}"`;
+    else if (action === 'fork')
+      title = `Forking "${name}"`;
+    else if (action === 'create')
+      title = `Creating from "${name}"`;
+
+    return (
+      <div>
+        <h2>{title}</h2>
+        <hr />
       </div>
     );
   }
