59473999e091833c8f178335d31f0dbbdaa07651
Yomguithereal <guillaumeplique@gmail.com>
2015-11-04 16:29:07 +0100

    Testing batches

--- test/suites/batch.js
+++ test/suites/batch.js
@@ -2,7 +2,7 @@ import assert from 'assert';
 import Batch from '../../lib/batch';
 
 describe('Neo4j Batches', function() {
-  it('should be possible to save new nodes.', function() {
+  it('should be possible to compile complex batches.', function() {
     const batch = new Batch();
 
     const dairy  = batch.save({name: 'Dairy products'}, ['ClassifiedItem', 'ClassifiedProduct']),
@@ -11,7 +11,38 @@ describe('Neo4j Batches', function() {
 
     batch.relate(dairy, 'AGGREGATES', milk);
     batch.relate(dairy, 'AGGREGATES', cheese);
+    batch.relate(45, 'AGGREGATES', cheese);
 
-    batch.commit();
+    batch.update(45, {note: 'Here you go.'});
+
+    const {query, params} = batch.compile();
+
+    assert.deepEqual(
+      query.split('\n'),
+      [
+        'START ne45=(45)',
+        'SET ne45 += {pe45}',
+        'CREATE (ni0 {pi0})',
+        'SET ni0:`ClassifiedItem`',
+        'SET ni0:`ClassifiedProduct`',
+        'CREATE (ni1 {pi1})',
+        'SET ni1:`Item`',
+        'CREATE (ni2 {pi2})',
+        'SET ni2:`Item`',
+        'CREATE (ni0)-[:`AGGREGATES`]->(ni1)',
+        'CREATE (ni0)-[:`AGGREGATES`]->(ni2)',
+        'CREATE (ne45)-[:`AGGREGATES`]->(ni2);'
+      ]
+    );
+
+    assert.deepEqual(
+      params,
+      {
+        pe45: {note: 'Here you go.'},
+        pi0: {name: 'Dairy products'},
+        pi1: {name: 'Milk'},
+        pi2: {name: 'Cheese'}
+      }
+    );
   });
 });
