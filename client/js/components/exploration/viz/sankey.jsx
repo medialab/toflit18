6de5fb405804fa91bd5c90f96ed349a243f29c42
gtible <gregory.tible@gmail.com>
2015-11-17 16:09:56 +0100

    update sankey

--- client/js/components/exploration/viz/sankey.jsx
+++ client/js/components/exploration/viz/sankey.jsx
@@ -10,25 +10,26 @@ import React, {Component} from 'react';
  * Helpers
  */
 
+
+
 /**
  * Sankey component
  */
 export default class Sankey extends Component {
   render() {
-    const {data, height, width} = this.props; 
+    const {data} = this.props; 
 
-    margin = {top: 1, right: 1, bottom: 6, left: 1},
-	width = document.querySelector('#chart').offsetWidth,
-	height = 600;
+  let width = 1000,
+  height = 600;
 
-	sankey = {},
-	nodeWidth = 10,
-	nodePadding = 8,
-	nodes = [],
-	links = [];
+  let sankey = {},
+    nodeWidth = 10,
+    nodePadding = 8,
+    nodes = [],
+    links = [];
 
     return (
-      <svg width="width" height="height"> </svg>
+      <svg width={width} height={height}> </svg>
     );
   }
 }
