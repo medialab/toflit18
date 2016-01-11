28fa775a77e1490ca0a552fcdee0a1885446485c
Yomguithereal <guillaumeplique@gmail.com>
2016-01-11 18:19:11 +0100

    Towards Babel 6

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,17 @@
 var webpack = require('webpack'),
     path = require('path');
 
+var PRESETS = [
+  'es2015',
+  'stage-0',
+  'react',
+  'react-hmre'
+];
+
+PRESETS.forEach(function(p, i) {
+  PRESETS[i] = require.resolve('babel-preset-' + p);
+});
+
 module.exports = {
   devtool: '#cheap-module-eval-source-map',
   entry: [
@@ -29,20 +40,24 @@ module.exports = {
       {
         test: /\.jsx?$/,
         include: [path.join(__dirname, 'js'), path.resolve(__dirname, '../lib')],
-        loaders: ['babel-loader']
+        loader: 'babel',
+        query: {
+          presets: PRESETS,
+          plugins: [require.resolve('babel-plugin-transform-decorators-legacy')]
+        }
       },
 
       // JSON configuration file
       {
         test: /\.json$/,
-        include: path.join(__dirname) + '/config.json',
-        loader: 'json-loader'
+        include: path.join(__dirname, 'config.json'),
+        loader: 'json'
       },
 
       // Fonts
       {
         test: /\.(ttf|eot|svg|woff2?)(\?[a-z0-9.=]+)?$/,
-        loader: 'file-loader'
+        loader: 'file'
       }
     ]
   }
