74557514db33513e1653391c4394297be8988f1c
Yomguithereal <guillaumeplique@gmail.com>
2016-01-25 16:49:15 +0100

    Adding a prod config for client build

---
+++ client/webpack.config.prod.js
@@ -0,0 +1,44 @@
+var webpack = require('webpack'),
+    path = require('path');
+
+var PRESETS = [
+  'es2015',
+  'stage-0',
+  'react'
+];
+
+PRESETS.forEach(function(p, i) {
+  PRESETS[i] = require.resolve('babel-preset-' + p);
+});
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './js/main.jsx',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/build/',
+    library: 'app'
+  },
+  module: {
+    loaders: [
+
+      // ES6 & JSX
+      {
+        test: /\.jsx?$/,
+        include: [path.join(__dirname, 'js'), path.resolve(__dirname, '../lib')],
+        loader: 'babel',
+        query: {
+          presets: PRESETS,
+          plugins: [require.resolve('babel-plugin-transform-decorators-legacy')]
+        }
+      },
+
+      // Fonts
+      {
+        test: /\.(ttf|eot|svg|woff2?)(\?[a-z0-9.=]+)?$/,
+        loader: 'file'
+      }
+    ]
+  }
+};
