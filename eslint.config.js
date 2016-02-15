4daa7d5ee88a5112ca1befb72c61c720a20f7987
Yomguithereal <guillaumeplique@gmail.com>
2016-02-15 18:17:14 +0100

    Moving some configs to package.json

--- eslint.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: [
-    '@yomguithereal/eslint-config/es7',
-    '@yomguithereal/eslint-config/react'
-  ].map(require.resolve),
-  globals: {
-    sigma: true
-  }
-};
