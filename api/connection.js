6ef5240d5bc2baf5d32fbfd4ab4b98d6d5b3d223
Yomguithereal <guillaumeplique@gmail.com>
2017-04-20 15:14:33 +0200

    Migration to v3

--- api/connection.js
+++ api/connection.js
@@ -4,15 +4,45 @@
  *
  * Connection to the project's Neo4j database.
  */
-import neo4j from 'neo4j';
 import {db as config} from '../config.json';
+const neo4j = require('neo4j-driver').v1;
 
-const DB = new neo4j.GraphDatabase({
-  auth: {
-    username: config.user,
-    password: config.password
-  },
-  url: `http://${config.host}:${config.port}`
-});
+// Authentication
+const auth = neo4j.auth.basic(config.user, config.password);
+
+// Driver
+const driver = neo4j.driver(`bolt://${config.host}:${config.port}`, auth);
+
+driver.onError = err => {
+  console.error('Neo4j driver error', err);
+  process.exit(1);
+};
+
+// Database abstraction
+const DB = {
+  cypher(body, callback) {
+    let query,
+        params = {};
+
+    if (typeof body === 'string') {
+      query = body;
+    }
+    else {
+      query = body.query;
+      params = body.params;
+    }
+
+    const session = driver.session('READ');
+
+    return session
+      .run(query, params)
+      .then(result => {
+        session.close();
+        return callback(null, result.records.map(record => record.toObject()));
+      })
+      .catch(err => callback(err));
+  }
+};
+
 
 export default DB;
