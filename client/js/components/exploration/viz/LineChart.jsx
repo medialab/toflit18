92cb9f0882fd18afcb83abaff6903cf1944033df
Yomguithereal <guillaumeplique@gmail.com>
2017-08-30 18:11:18 +0200

    Starting recharts conversion

--- client/js/components/exploration/viz/LineChart.jsx
+++ client/js/components/exploration/viz/LineChart.jsx
@@ -5,225 +5,121 @@
  * Basic line chart component describing flows along the years.
  */
 import React, {Component} from 'react';
-import Tooltip from 'rc-tooltip';
-import measured from '@yomguithereal/react-utilities/measured';
-import {format} from 'd3-format';
-import {scaleLinear as linear} from 'd3-scale';
-import * as shape from 'd3-shape';
-import {flatten, minBy, maxBy} from 'lodash';
-import {prettyPrint} from '../../../lib/helpers';
+import {format, formatPrefix} from 'd3-format';
+import {
+  ResponsiveContainer,
+  LineChart as RechartsLineChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Line
+} from 'recharts';
 
 /**
  * Formats.
  */
-const AXIS_FORMAT = format(',');
+const Y_AXIS_FORMAT = formatPrefix(',.0s', 1.3e3);
+const NUMBER_FORMAT = format(',');
+
+/**
+ * Custom tooltip.
+ */
+const UNITS = {
+  count: 'flows',
+  value: 'lt.'
+};
+
+const renderTooltip = valueKey => data => {
+  const payload = data.payload.slice().sort((a, b) => {
+    return b.value - a.value;
+  });
+
+  const style = {
+    margin: 0,
+    padding: 10,
+    backgroundColor: '#fff',
+    border: '1px solid #ccc',
+    whiteSpace: 'nowrap'
+  };
+
+  return (
+    <div style={style}>
+      <em className="recharts-tooltip-label">{data.label}</em>
+      <ul style={{listStyleType: 'none', padding: '0px', margin: '0px'}}>
+        {payload.map(item => {
+          return (
+            <li key={item.name}>
+              <span style={{color: item.color}}>{NUMBER_FORMAT(item.value | 0)} {UNITS[valueKey]}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 /**
  * Main component.
  */
-@measured({width: '100%'})
 export default class LineChart extends Component {
   render() {
     const {
       data,
-      valueKey = 'value',
-      width: fullWidth
+      valueKey = 'value'
     } = this.props;
 
-    if (!data.length) {
+    if (!data || !data.length)
       return null;
-    }
 
-    const lines = data.map(line => {
-      return line
-        .data
-        .filter(row => {
-            return row.count !== null && row.value !== null;
-          })
-        .map((row) => {
-          if (row.count !== null && row.value !== null) {
-            return {
-              year: row.year,
-              value: row[valueKey]
-            };
-          }
-        });
+    const minYear = Math.min.apply(null, data.map(line => line.data[0].year));
+
+    const maxYear = Math.max.apply(null, data.map(line => line.data[line.data.length - 1].year));
+
+    const lineData = new Array(maxYear - minYear + 1);
+
+    const hash = year => year - minYear;
+
+    for (let i = 0, l = lineData.length; i < l; i++)
+      lineData[i] = {year: minYear + i};
+
+    data.forEach((line, i) => {
+      for (let j = 0, m = line.data.length; j < m; j++) {
+        const h = hash(line.data[j].year);
+        lineData[h][i] = line.data[j][valueKey];
+      }
     });
 
-    const palette = data.map(line => line.color);
-
-    const margin = {
-      top: 10,
-      right: 10,
-      left: 100,
-      bottom: 30
-    };
-
-    const height = 180;
-
-    const width = fullWidth - margin.left - margin.right,
-          fullHeight = height + margin.top + margin.bottom;
-
-    // Computing max values
-    const fullData = flatten(lines);
-
-    const minYear = minBy(fullData, d => d.year).year,
-          maxYear = maxBy(fullData, d => d.year).year,
-          maxValue = maxBy(fullData, d => d.value).value;
-
-    // Building scales
-    const x = linear()
-      .domain([minYear, maxYear])
-      .range([0, width]);
-
-    const y = linear()
-      .domain([0, maxValue])
-      .range([height, 0]);
-
-    // Shapes
-    const lineShape = shape.line()
-      .x(d => x(d.year))
-      .y(d => y(d.value));
-
-    function renderLines(points, i) {
-      const parts = points.reduce(function(acc, point) {
-        const lastPart = acc[acc.length - 1] || [],
-              lastItem = lastPart[lastPart.length - 1];
-
-        if (lastItem && (point.year - lastItem.year) <= 5) {
-          lastPart.push(point);
-        }
-        else {
-          acc.push([point]);
-        }
-
-        return acc;
-      }, []);
-
-      return parts
-        .filter(part => part.length > 1)
-        .map(function(part, key) {
-
-          // Rendering a whole series
-          return <path key={key} stroke={palette[i]} d={lineShape(part)} />;
-        });
-    }
-
-    function renderPoint(point, i) {
-
-      const overlay = (
-        <span>
-          {prettyPrint(point.value)} ({point.year})
-        </span>
-      );
-
-      // Rendering a single point in time
-      return (
-        <Tooltip
-          placement="top"
-          align={{offset: [3, 0]}}
-          overlay={overlay}
-          mouseLeaveDelay={0}>
-          <circle
-            cx={x(point.year)}
-            cy={y(point.value)}
-            r={2}
-            fill="white"
-            stroke={palette[i]} />
-        </Tooltip>
-      );
-    }
-
     return (
-      <svg width="100%" height={fullHeight} className="line-chart">
-        <XAxis
-          width={fullWidth}
-          height={fullHeight}
-          margin={margin}
-          scale={x} />
-        <YAxis
-          width={fullWidth}
-          height={fullHeight}
-          margin={margin}
-          scale={y} />
-        <g className="points" transform={`translate(${margin.left}, ${margin.top})`}>
-          {lines.map(renderLines)}
-          {lines.map((line, i) => line.map(row => renderPoint(row, i)))}
-        </g>
-      </svg>
-    );
-  }
-}
-
-/**
- * X axis.
- */
-class XAxis extends Component {
-  render() {
-    const {margin, width, height, scale} = this.props;
-
-    const bottom = height - margin.bottom;
-
-    const ticks = scale.ticks();
-
-    function renderTick(t, i) {
-      const left = margin.left + scale(t);
-
-      return (
-        <g key={i} className="tick" transform={`translate(${left}, ${bottom})`}>
-          <line y2={5} />
-          <text y={15} textAnchor="middle">
-            {t}
-          </text>
-        </g>
-      );
-    }
-
-    return (
-      <g className="x axis">
-        <line
-          x1={margin.left}
-          x2={width - margin.right}
-          y1={bottom}
-          y2={bottom} />
-        {ticks.map(renderTick)}
-      </g>
-    );
-  }
-}
-
-/**
- * Y axis.
- */
-class YAxis extends Component {
-  render() {
-    const {margin, width, height, scale} = this.props;
-
-    const ticks = scale.ticks().filter(Number.isInteger);
-
-    function renderTick(t, i) {
-      const top = (margin.top + scale(t));
-
-      return (
-        <g key={i} className="tick" transform={`translate(${margin.left}, ${top})`}>
-          <line x1={-5} x2={0} />
-          <text x={-7} y="0.32em" textAnchor="end">
-            {AXIS_FORMAT(t)}
-          </text>
-          <line className="dotted" x2={width - margin.right - margin.left} />
-        </g>
-      );
-    }
-
-    return (
-      <g className="y axis">
-        <line
-          x1={margin.left}
-          x2={margin.left}
-          y1={margin.top}
-          y2={height - margin.bottom} />
-        {ticks.filter(d => !!d).map(renderTick)}
-      </g>
+      <div className="line-chart">
+        <ResponsiveContainer width="100%" height={250}>
+          <RechartsLineChart
+            syncId="line-chart"
+            data={lineData}
+            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis
+              dataKey="year"
+              domain={[minYear, maxYear]}
+              interval={10} />
+            <YAxis tickFormatter={Y_AXIS_FORMAT} />
+            <Tooltip
+              isAnimationActive={false}
+              content={renderTooltip(valueKey)} />
+            {data.map((line, i) => {
+              return (
+                <Line
+                  key={i}
+                  dataKey={i}
+                  stroke={line.color}
+                  isAnimationActive={false}
+                  dot={{r: 2, stroke: line.color, strokeWidth: 1}}
+                  activeDot={{r: 4, stroke: 'white', strokeWidth: 2, fill: line.color}} />
+              );
+            })}
+          </RechartsLineChart>
+        </ResponsiveContainer>
+      </div>
     );
   }
 }
