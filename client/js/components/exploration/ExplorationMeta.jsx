679820ad22ac82bac81b56ba3acde4f3ce411549
Alexis Jacomy <alexis.jacomy@gmail.com>
2017-11-13 17:09:11 +0100

    [client] Draft new ExplorationMeta view

--- client/js/components/exploration/ExplorationMeta.jsx
+++ client/js/components/exploration/ExplorationMeta.jsx
@@ -6,14 +6,14 @@
  * themselves and how they interact with each other.
  */
 import React, {Component} from 'react';
-import Button, {ExportButton} from '../misc/Button.jsx';
 import {Waiter} from '../misc/Loaders.jsx';
 import {ClassificationSelector, ItemSelector} from '../misc/Selectors.jsx';
 import {branch} from 'baobab-react/decorators';
 import DataQualityBarChart from './viz/DataQualityBarChart.jsx';
 import SourcesPerDirections from './viz/SourcesPerDirections.jsx';
 
-import {Row, Col} from '../misc/Grid.jsx';
+import {exportCSV} from '../../lib/exports';
+import VizLayout from '../misc/VizLayout.jsx';
 import {prettyPrint} from '../../lib/helpers';
 
 import specs from '../../../specs.json';
@@ -85,6 +85,21 @@ function formatArrayToCSV(data) {
   }
 })
 export default class ExplorationMeta extends Component {
+  exportPerYear() {
+    const {state} = this.props;
+    exportCSV({
+      data: state.perYear,
+      name: `Toflit18_Meta_view ${state.dataType.name} - ${state.fileName} data_per_year.csv`,
+    });
+  }
+  exportFlows() {
+    const {state} = this.props;
+    exportCSV({
+      data: formatArrayToCSV(state.flowsPerYear || []),
+      name: `Toflit18_Meta_view ${state.dataType.name} - ${state.fileName} flows_per_year.csv`,
+    });
+  }
+
   render() {
     const {
       actions,
@@ -157,54 +172,49 @@ export default class ExplorationMeta extends Component {
       childClassifications = getChildClassifications(classificationIndex, state.dataType);
 
     return (
-      <div>
-        <div className="panel">
-          <h4>Metadata</h4>
-          <p>
-            <em>Some information about the data itself.</em>
-          </p>
-          <h6 className="section-separator">What we want information about:</h6>
-          <Row className="dataType">
-           <SectionTitle
-             title="Data type"
-             addendum="You must select the type of data to control." />
-            <Col md={6}>
-              <ItemSelector
-                type="dataType"
-                data={[...metadataSelectors, ...classificationsFiltered]}
-                loading={!classifications.product.length}
-                onChange={actions.select}
-                selected={state.dataType} />
-            </Col>
-          </Row>
-          <h6 className="section-separator">Filters:</h6>
-          <Row>
-            <SectionTitle
-              title="Source Type"
-              addendum="From which sources does the data comes from?" />
-            <Col md={4}>
-              <ItemSelector
-                type="sourceType"
-                data={sourceTypesOptions}
-                loading={!sourceTypesOptions.length}
-                onChange={actions.update.bind(null, 'sourceType')}
-                selected={selectors.sourceType} />
-            </Col>
-          </Row>
-          <hr />
-          <Row>
-            <SectionTitle
-              title={(state.dataType && state.dataType.model === 'product') ? 'Child product' : 'Product'}
-              addendum="The type of product being shipped." />
-            <Col md={4}>
+      <VizLayout
+        title="Metadata"
+        description="Some information about the data itself."
+        leftPanelName="Filters"
+        rightPanelName="Caption" >
+        { /* Top of the left panel */ }
+        <div className="box-selection box-selection-lg">
+          <h2 className="hidden-xs"><span className="hidden-sm hidden-md">The type of </span><span>data</span></h2>
+          <div className="form-group">
+            <label htmlFor="classifications" className="control-label sr-only">Type of data</label>
+            <ItemSelector
+              type="dataType"
+              data={[...metadataSelectors, ...classificationsFiltered]}
+              loading={!classifications.product.length}
+              onChange={actions.select}
+              selected={state.dataType} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="classifications" className="control-label">Source Type</label>
+            <ItemSelector
+              type="sourceType"
+              data={sourceTypesOptions}
+              loading={!sourceTypesOptions.length}
+              onChange={actions.update.bind(null, 'sourceType')}
+              selected={selectors.sourceType} />
+          </div>
+        </div>
+
+        { /* Left panel */ }
+        <div className="aside-filters">
+          <h3>Filters</h3>
+          <form onSubmit={e => e.preventDefault()}>
+            <div className="form-group">
+              <label htmlFor="product" className="control-label">{
+                (state.dataType && state.dataType.model === 'product') ? 'Child product' : 'Product'
+              }</label>
+              <small className="help-block">The type of product being shipped.</small>
               <ClassificationSelector
                 type="product"
                 loading={!classifications.product.length}
                 data={childClassifications || classifications.product.filter(c => !c.source)}
                 onChange={actions.update.bind(null, 'productClassification')}
                 selected={selectors.productClassification} />
-            </Col>
-            <Col md={4}>
               <ItemSelector
                 type="product"
                 disabled={!selectors.productClassification || !groups.product.length}
@@ -212,22 +222,18 @@ export default class ExplorationMeta extends Component {
                 data={groups.product}
                 onChange={actions.update.bind(null, 'product')}
                 selected={selectors.product} />
-            </Col>
-          </Row>
-          <hr />
-          <Row>
-            <SectionTitle
-              title={(state.dataType && state.dataType.model === 'country') ? 'Child country' : 'Country'}
-              addendum="The country whence we got the products or wither we are sending them." />
-            <Col md={4}>
+            </div>
+            <div className="form-group">
+              <label htmlFor="country" className="control-label">{
+                (state.dataType && state.dataType.model === 'country') ? 'Child country' : 'Country'
+              }</label>
+              <small className="help-block">The country whence we got the products or wither we are sending them.</small>
               <ClassificationSelector
                 type="country"
                 loading={!classifications.country.length}
                 data={childClassifications || classifications.country.filter(c => !c.source)}
                 onChange={actions.update.bind(null, 'countryClassification')}
                 selected={selectors.countryClassification} />
-            </Col>
-            <Col md={4}>
               <ItemSelector
                 type="country"
                 disabled={!selectors.countryClassification || !groups.country.length}
@@ -235,94 +241,86 @@ export default class ExplorationMeta extends Component {
                 data={groups.country}
                 onChange={actions.update.bind(null, 'country')}
                 selected={selectors.country} />
-            </Col>
-          </Row>
-          <hr />
-          <Row>
-            <SectionTitle
-              title="Direction"
-              addendum="The French harbor where the transactions were recorded." />
-            <Col md={4}>
+            </div>
+            <div className="form-group">
+              <label htmlFor="direction" className="control-label">Direction</label>
+              <small className="help-block">The French harbor where the transactions were recorded.</small>
               <ItemSelector
                 type="direction"
                 loading={!directions}
                 data={directions || []}
                 onChange={actions.update.bind(null, 'direction')}
                 selected={selectors.direction} />
-            </Col>
-          </Row>
-          <hr />
-          <Row>
-            <SectionTitle
-              title="Kind"
-              addendum="Should we look at import, export, or total?" />
-            <Col md={4}>
+            </div>
+            <div className="form-group">
+              <label htmlFor="kind" className="control-label">Kind</label>
+              <small className="help-block">Should we look at import, export, or total?</small>
               <ItemSelector
                 type="kind"
                 onChange={actions.update.bind(null, 'kind')}
                 selected={selectors.kind} />
-            </Col>
-          </Row>
-          <hr />
-          <Row>
-          <Col md={2}>
-            <Button
-              kind="primary"
-              disabled={!state.dataType}
-              loading={state.loading}
-              onClick={actions.addChart}>
-              Add Charts
-            </Button>
-          </Col>
-        </Row>
+            </div>
+            <div className="form-group-fixed">
+              <button
+                type="submit"
+                className="btn btn-default"
+                data-loading={state.loading}
+                disabled={!state.dataType}
+                onClick={actions.addChart}>
+                Update
+              </button>
+            </div>
+          </form>
         </div>
-        {state.perYear && state.dataType && <div className="panel">
-          {state.perYear ?
-            <div>
-              <p>Total number of {unit} per year</p>
-              <DataQualityBarChart
-                yAxis
-                data={barData}
-                unit={unit}
-                syncId="sources-per-directions" />
-            </div> :
-            <Waiter />}
-            <ExportButton
-              name={`Toflit18_Meta_view ${state.dataType.name} - ${state.fileName} data_per_year`}
-              data={state.perYear}>
-              Export
-            </ExportButton>
-        </div>}
-        {canDisplaySecondViz && state.flowsPerYear && state.dataType && <div className="panel">
-          {state.flowsPerYear ?
-            <SourcesPerDirections data={state.flowsPerYear} /> :
-            <Waiter />}
-            <ExportButton
-              name={`Toflit18_Meta_view ${state.dataType.name} - ${state.fileName} flows_per_year`}
-              data={formatArrayToCSV(state.flowsPerYear || [])}>
-              Export
-            </ExportButton>
-        </div>}
-      </div>
+
+        { /* Content panel */ }
+        <div className="col-xs-12 col-sm-6 col-md-8">
+          <div className="viz-data">
+            {state.perYear && state.dataType && (
+              <div className="box-viz">
+                {state.perYear ?
+                  <div>
+                    <p>Total number of {unit} per year</p>
+                    <DataQualityBarChart
+                      yAxis
+                      data={barData}
+                      unit={unit}
+                      syncId="sources-per-directions" />
+                  </div> :
+                  <Waiter />}
+                <button
+                  type="submit"
+                  className="btn btn-default"
+                  onClick={() => this.exportPerYear()}>
+                  Export
+                </button>
+              </div>
+            )}
+            {canDisplaySecondViz && state.flowsPerYear && state.dataType && (
+              <div className="box-viz">
+                {state.flowsPerYear ?
+                  <SourcesPerDirections data={state.flowsPerYear} /> :
+                  <Waiter />}
+                <button
+                  type="submit"
+                  className="btn btn-default"
+                  onClick={() => this.exportFlows()}>
+                  Export
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+
+        { /* Right panel */ }
+        <div className="aside-legend">
+          <ul className="list-unstyled list-legend">
+            <li><span style={{backgroundColor: '#8d4d42'}} />Number direction</li>
+            <li><span style={{backgroundColor: '#4F7178'}} />Number of flows</li>
+          </ul>
+          <p>Barcharts are sorted by average number of flows per year.</p>
+        </div>
+      </VizLayout>
     );
   }
 }
-
-/**
- * Section title.
- */
-class SectionTitle extends Component {
-  render() {
-    const {title, addendum} = this.props;
-
-    return (
-      <Col md={4}>
-        <div className="section-title">{title}</div>
-        <div className="section-explanation">
-          <em>{addendum}</em>
-        </div>
-      </Col>
-    );
-  }
-}
-
