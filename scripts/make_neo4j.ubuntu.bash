e9d65cc74c21798d9b69af8757caa02935df3a92
Paul Girard <paul.girard@sciencespo.fr>
2019-03-15 11:47:42 +0100

    update build for ubuntu with sleep

--- scripts/make_neo4j.ubuntu.bash
+++ scripts/make_neo4j.ubuntu.bash
@@ -15,8 +15,8 @@ TOFLIT18_NEO4J_PATH=/home/pgi/dev/toflit18/neo4j
 npm run import -- -- --path ../toflit18_data/
 
 # Stopping the neo4j service
-bash $TOFLIT18_NEO4J_PATH/bin/neo4j stop
-
+bash $TOFLIT18_NEO4J_PATH/bin/neo4j stop &&
+echo 'neo4j stopped'
 # Erasing the current database
 rm -rf $TOFLIT18_NEO4J_PATH/data/databases/graph.db &&
 
@@ -25,14 +25,15 @@ rm -rf graph.db &&
 bash $TOFLIT18_NEO4J_PATH/bin/neo4j-import --into graph.db --nodes ./.output/nodes.csv --relationships ./.output/edges.csv &&
 
 # Replacing the database
-sudo mv graph.db $TOFLIT18_NEO4J_PATH/data/databases/ &&
-#sudo chown -R neo4j:adm $TOFLIT18_NEO4J_PATH/data/databases/graph.db &&
+mv graph.db $TOFLIT18_NEO4J_PATH/data/databases/ &&
+#sudo chown -R neo4j:neo4j $TOFLIT18_NEO4J_PATH/data/databases/graph.db &&
 
 # Restarting the databse
-bash $TOFLIT18_NEO4J_PATH/bin/neo4j start
-
+bash $TOFLIT18_NEO4J_PATH/bin/neo4j start &&
+echo 'neo4j started' &&
+sleep 3 &&
 # Building indices
-npm run indices
+npm run indices &&
 
 # quantities
 npm run quantities  -- -- --path ../toflit18_data/
