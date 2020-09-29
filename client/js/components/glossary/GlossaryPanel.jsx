4ac0dc4c36319ad18ffd2d66fce87dd513b73bb5
Benoit Simard <contact@bsimard.com>
2020-09-29 13:53:07 +0200

    Retrieve JSON glossary with ajax

--- client/js/components/glossary/GlossaryPanel.jsx
+++ client/js/components/glossary/GlossaryPanel.jsx
@@ -8,28 +8,13 @@
 import React, {Component} from 'react';
 import {escapeRegexp} from 'talisman/regexp';
 import cls from 'classnames';
-import RAW_GLOSSARY_DATA from '../../../glossary.json';
-
+import ajax from 'djax';
 import VizLayout from '../misc/VizLayout.jsx';
 
 /**
  * Constants.
  */
 const URL_REGEX = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/gi;
-
-const GLOSSARY_DATA = RAW_GLOSSARY_DATA.map(entry => {
-  return {
-    ...entry,
-    html: entry.definition.replace(URL_REGEX, match => {
-      let label = match;
-
-      if (match.length > 70)
-        label = match.slice(0, 67) + '...';
-
-      return `<a href="${match}">${label}</a>`;
-    })
-  };
-});
 
 /**
  * Component representing a single glossary entry.
@@ -52,40 +37,53 @@ export default class GlossaryPanel extends Component {
 
     this.state = {
       query: '',
-      entries: GLOSSARY_DATA
+      entries: [],
+      glossary: []
     };
 
     this.handleInput = this.handleInput.bind(this);
   }
 
+  componentDidMount() {
+    ajax({url: CONFIG.glossary_url}).then(data => {
+      const glossary_data = data.map(entry => {
+        return {
+          ...entry,
+          html: entry.definition.replace(URL_REGEX, match => {
+            let label = match;
+            if (match.length > 70) label = match.slice(0, 67) + '...';
+            return `<a href="${match}">${label}</a>`;
+          })
+        };
+      });
+      this.setState({
+        glossary: glossary_data,
+        entries: glossary_data
+      });
+    });
+  }
+
   handleInput(e) {
     const query = e.target.value;
 
-    if (!query && this.state.entries.length !== GLOSSARY_DATA) {
-      this.setState({query, entries: GLOSSARY_DATA});
-    }
-    else {
+    if (!query && this.state.entries.length !== this.state.glossary) {
+      this.setState({query, entries: this.state.glossary});
+    } else {
       this.setState({query});
 
-      if (query.length > 2)
-        this.performSearch(query);
-      else
-        this.performSearch('');
+      if (query.length > 2) this.performSearch(query);
+      else this.performSearch('');
     }
   }
 
   performSearch(query) {
     if (!query) {
-      this.setState({entries: GLOSSARY_DATA.slice(0)});
-    }
-    else {
+      this.setState({entries: this.state.glossary.slice(0)});
+    } else {
       const pattern = new RegExp(escapeRegexp(query));
 
-      const filteredEntries = GLOSSARY_DATA.filter(entry => {
-        return (
-          pattern.test(entry.name) ||
-          pattern.test(entry.definition)
-        );
+      const filteredEntries = this.state.glossary.filter(entry => {
+        return pattern.test(entry.name) || pattern.test(entry.definition);
       });
 
       this.setState({entries: filteredEntries});
@@ -96,10 +94,7 @@ export default class GlossaryPanel extends Component {
     const {query} = this.state;
     const entries = this.state.entries.map(entry => {
       return (
-        <GlossaryEntry
-          key={entry.name}
-          name={entry.name}
-          html={entry.html} />
+        <GlossaryEntry key={entry.name} name={entry.name} html={entry.html} />
       );
     });
 
@@ -107,14 +102,12 @@ export default class GlossaryPanel extends Component {
       <VizLayout
         title="Glossary"
         description="This glossary gives identification and definitions of selected commodities that were exchanged between France and its economic partners. If you want to contribute a definition, or correct a mistake, please send an email to guillaume.daudin@dauphine.psl.eu"
-        leftPanelName="Search" >
-        { /* Top of the left panel */ }
+        leftPanelName="Search">
+        {/* Top of the left panel */}
 
-        { /* Left panel */ }
+        {/* Left panel */}
         <div className="aside-filters">
-          <label
-            htmlFor="classifications"
-            className="control-label">
+          <label htmlFor="classifications" className="control-label">
             Search
           </label>
           <form onSubmit={e => e.preventDefault()}>
@@ -125,19 +118,24 @@ export default class GlossaryPanel extends Component {
                 className="form-control"
                 placeholder="Search products..."
                 value={query}
-                onChange={this.handleInput} />
+                onChange={this.handleInput}
+              />
             </div>
             <div className="form-group">
-            <p className={cls((!entries.length || query.length <= 2) && 'hidden')}>
-              We found <strong>{entries.length}</strong> results found for "{query}"
-            </p>
-            <p className={cls(entries.length && 'hidden')}>
-              We're sorry. We cannot find any matches for your search.
-            </p>
+              <p
+                className={cls(
+                  (!entries.length || query.length <= 2) && 'hidden'
+                )}>
+                We found <strong>{entries.length}</strong> results found for "
+                {query}"
+              </p>
+              <p className={cls(entries.length && 'hidden')}>
+                We're sorry. We cannot find any matches for your search.
+              </p>
             </div>
           </form>
         </div>
-        { /* Content panel */ }
+        {/* Content panel */}
         <div className="content-viz">
           <div className="col-xs-12 col-sm-6 col-md-8">
             <div className="row">
