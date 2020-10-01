35113f4e3ab2370d0abd4d152865b2ae244df48f
Paul Girard <paul@ouestware.com>
2020-10-01 18:20:36 +0200

    white background for SVG export

--- client/js/lib/exports.js
+++ client/js/lib/exports.js
@@ -1,33 +1,33 @@
-import gexf from 'gexf';
-import {sum} from 'lodash';
-import d2i from 'dom-to-image';
-import csvParse from 'papaparse';
-import {saveAs} from 'browser-filesaver';
+import gexf from "gexf";
+import { sum } from "lodash";
+import d2i from "dom-to-image";
+import csvParse from "papaparse";
+import { saveAs } from "browser-filesaver";
 
-export function exportGEXF({data, meta, model, name, params}) {
+export function exportGEXF({ data, meta, model, name, params }) {
   const gexfParams = {
     meta,
     model,
     nodes: data.nodes,
     edges: data.edges,
-    version: '0.0.1',
+    version: "0.0.1",
     ...params,
   };
 
   const writer = gexf.create(gexfParams);
-  const blob = new Blob([writer.serialize()], {type: 'text/gexf+xml;charset=utf-8'});
+  const blob = new Blob([writer.serialize()], { type: "text/gexf+xml;charset=utf-8" });
 
   return saveAs(blob, name);
 }
 
-export function exportCSV({data, name}) {
+export function exportCSV({ data, name }) {
   const csv = csvParse.unparse(data);
-  const blob = new Blob([csv], {type: 'text/csv;charset=utf-8'});
+  const blob = new Blob([csv], { type: "text/csv;charset=utf-8" });
 
   return saveAs(blob, name);
 }
 
-export function exportSVG({nodes, name}) {
+export function exportSVG({ nodes, name }) {
   const domNodes = Array.isArray(nodes) ? nodes : [nodes];
   const svgs = [];
   const todos = domNodes.length;
@@ -42,15 +42,16 @@ export function exportSVG({nodes, name}) {
 
       return svg
         .replace(/^<svg[^>]+>/, `<g transform="translate(0 ${sum(heights.slice(0, -1))})" x="0" y="0">`)
-        .replace(/<\/svg>$/, '</g>');
+        .replace(/<\/svg>$/, "</g>");
     });
 
     const finalSvg = [
-      `<svg xmlns="http://www.w3.org/2000/svg" width="${Math.max(...widths) + 50}px" height="${sum(heights) + 50}px">`,
+      `<svg xmlns="http://www.w3.org/2000/svg" width="${Math.max(...widths) + 50}px" height="${sum(heights) + 50}px">
+      <rect width="100%" height="100%" fill="white"/>`,
       ...contents,
-      '</svg>',
-    ].join('');
-    const blob = new Blob([finalSvg], {type: 'text/svg;charset=utf-8'});
+      "</svg>",
+    ].join("");
+    const blob = new Blob([finalSvg], { type: "text/svg;charset=utf-8" });
 
     return saveAs(blob, name);
   }
@@ -58,15 +59,15 @@ export function exportSVG({nodes, name}) {
   Promise.all(
     domNodes.map((node, i) => {
       return new Promise((resolve, reject) => {
-        if (typeof node === 'string') {
+        if (typeof node === "string") {
           svgs[i] = node
-            .replace(/^<\?xml[^>]+>/, '')
-            .replace(/<\!DOCTYPE[^>]+>/, '')
-            .replace(/^\n\n/, '');
+            .replace(/^<\?xml[^>]+>/, "")
+            .replace(/<\!DOCTYPE[^>]+>/, "")
+            .replace(/^\n\n/, "");
           resolve();
         } else {
           d2i.toSvg(node).then(dataUrl => {
-            svgs[i] = dataUrl.replace(/^[^<]*</, '<');
+            svgs[i] = dataUrl.replace(/^[^<]*</, "<");
             resolve();
           });
         }
