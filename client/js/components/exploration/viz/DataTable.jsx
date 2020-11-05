dbcee0d68d39658a0f93e5332256ae8aed6cb7ed
Paul Girard <paul@ouestware.com>
2020-11-05 11:23:08 +0100

    removing copy paste and disable cell selection fixes #203

--- client/js/components/exploration/viz/DataTable.jsx
+++ client/js/components/exploration/viz/DataTable.jsx
@@ -6,7 +6,6 @@
  * partners and directions.
  */
 import React, {Component} from 'react';
-import {keys, max, values, range} from 'lodash'
 import {format, formatPrefix} from 'd3-format';
 import {
   updateSelector
@@ -65,63 +64,8 @@ export default class FlowsTable extends Component {
     super(props, context);
     this.rowHeight = 25;
     this.headerRowHeight= 50;
-    this.state = {
-      topLeft: {},
-      botmRight: {},
-    };
-    
   }
-  //disabling copy paste feature cause of issue https://github.com/medialab/toflit18/issues/203
-  // componentDidMount= () => {
-  //   document.addEventListener("copy", this.copy);
-  // }
-  // componentWillUnmount= () =>{
-  //   document.removeEventListener("copy", this.copy);
-  // }
 
-  // setSelection = args => {
-  //   this.setState({
-  //     topLeft: {
-  //       rowIdx: args.topLeft.rowIdx,
-  //       colIdx: args.topLeft.idx
-  //     },
-  //     botmRight: {
-  //       rowIdx: args.bottomRight.rowIdx,
-  //       colIdx: args.bottomRight.idx
-  //     }
-  //   });
-  // };
-  
-  // copy = (e) => {
-
-  //   const { topLeft, botmRight } = this.state;
-
-  //   if (topLeft.colIdx != -1 && botmRight.colIdx != -1){
-  //     e.preventDefault();
-  //     // Loop through each row
-  //     const columns = range(topLeft.colIdx -1, botmRight.colIdx)
-  //     .map(i => this.props.columnsOrder[i])
-  //     console.log(columns, this.props.columnsOrder, topLeft.colIdx, botmRight.colIdx );
-  //     const copyData =
-  //       // headers
-  //       [columns].concat(
-  //       // data
-  //       range(topLeft.rowIdx, botmRight.rowIdx + 1)
-  //       .map(
-  //         // Loop through each column
-  //         rowIdx =>
-  //           columns
-  //             .map(
-  //               // Grab the row values
-  //               col => this.props.flows[rowIdx][col]
-  //             )
-  //       )
-  //     );
-  //     const csv = csvParse.unparse(copyData)
-      
-  //     e.clipboardData.setData("text/plain", csv);
-  //   }
-  // }
 
   onHeaderClick(e){
     const {key} = e;
@@ -231,6 +175,7 @@ export default class FlowsTable extends Component {
           },
           ...columnsSpecificOpts[o.id]}
         }) : [];
+      
       return (
         <DraggableContainer onHeaderDrop={this.onHeaderDrop}>
              <ReactDataGrid
@@ -240,10 +185,7 @@ export default class FlowsTable extends Component {
               rowHeight={this.rowHeight}
               headerRowHeight={this.headerRowHeight}
               minHeight={900}
-              // cellRangeSelection={{
-              //   onComplete: this.setSelection
-              // }}
-              
+              enableCellSelect={false}  
             />
             </DraggableContainer>);
     };
