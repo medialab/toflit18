5cb06539e279a2fa6d95db0fc7936ee798aca3d1
Paul Girard <paul@ouestware.com>
2023-02-27 13:24:39 +0100

    [Client] reset size when removing column relates #212

--- client/js/components/exploration/viz/DataTable.jsx
+++ client/js/components/exploration/viz/DataTable.jsx
@@ -252,8 +252,9 @@ export default class FlowsTable extends Component {
           headerRowHeight={this.headerRowHeight}
           minHeight={800}
           enableCellSelect={false}
+          minColumnWidth={50}
           onColumnResize={(e, width) => {
-            if (width > 50) updateSelector("columnsWidth", { ...selectors.columnsWidth, [columns[e].key]: width });
+            updateSelector("columnsWidth", { ...selectors.columnsWidth, [columns[e].key]: width });
           }}
         />
       </DraggableContainer>
