7fa1d1674540ec661f965d34b91e03b0c5a02f49
Paul Girard <paul@ouestware.com>
2023-04-14 11:05:36 +0200

    [Client] prettie/linter alignement The code base still needs a global prettier run

--- client/js/lib/exports.js
+++ client/js/lib/exports.js
@@ -1,36 +1,36 @@
-import gexf from "gexf";
-import { sum } from "lodash";
-import d2i from "dom-to-image";
-import csvParse from "papaparse";
-import { saveAs } from "browser-filesaver";
+import gexf from 'gexf';
+import {sum} from 'lodash';
+import d2i from 'dom-to-image';
+import csvParse from 'papaparse';
+import {saveAs} from 'browser-filesaver';
 
-export function exportGEXF({ data, meta, model, name, params }) {
+export function exportGEXF({data, meta, model, name, params}) {
   const gexfParams = {
     meta,
     model,
     nodes: data.nodes,
     edges: data.edges,
-    version: "0.0.1",
+    version: '0.0.1',
     ...params,
   };
 
   const writer = gexf.create(gexfParams);
-  const blob = new Blob([writer.serialize()], { type: "text/gexf+xml;charset=utf-8" });
+  const blob = new Blob([writer.serialize()], {type: 'text/gexf+xml;charset=utf-8'});
 
   return saveAs(blob, name);
 }
 
-export function exportCSV({ data, name }) {
+export function exportCSV({data, name}) {
   const csv = csvParse.unparse(data);
-  const blob = new Blob([csv], { type: "text/csv;charset=utf-8" });
+  const blob = new Blob([csv], {type: 'text/csv;charset=utf-8'});
 
   return saveAs(blob, name);
 }
 
-export function exportSVG({ nodes, name }) {
+export function exportSVG({nodes, name}) {
   const domNodes = Array.isArray(nodes) ? nodes : [nodes];
   const svgs = [];
-  const todos = domNodes.length;
+  //const todos = domNodes.length;
 
   function finalize() {
     const widths = [];
@@ -42,32 +42,32 @@ export function exportSVG({ nodes, name }) {
 
       return svg
         .replace(/^<svg[^>]+>/, `<g transform="translate(0 ${sum(heights.slice(0, -1))})" x="0" y="0">`)
-        .replace(/<\/svg>$/, "</g>");
+        .replace(/<\/svg>$/, '</g>');
     });
 
     const finalSvg = [
       `<svg xmlns="http://www.w3.org/2000/svg" width="${Math.max(...widths) + 50}px" height="${sum(heights) + 50}px">
       <rect width="100%" height="100%" fill="white"/>`,
       ...contents,
-      "</svg>",
-    ].join("");
-    const blob = new Blob([finalSvg], { type: "text/svg;charset=utf-8" });
+      '</svg>',
+    ].join('');
+    const blob = new Blob([finalSvg], {type: 'text/svg;charset=utf-8'});
 
     return saveAs(blob, name);
   }
 
   Promise.all(
     domNodes.map((node, i) => {
-      return new Promise((resolve, reject) => {
-        if (typeof node === "string") {
+      return new Promise(resolve => {
+        if (typeof node === 'string') {
           svgs[i] = node
-            .replace(/^<\?xml[^>]+>/, "")
-            .replace(/<\!DOCTYPE[^>]+>/, "")
-            .replace(/^\n\n/, "");
+            .replace(/^<\?xml[^>]+>/, '')
+            .replace(/<\!DOCTYPE[^>]+>/, '')
+            .replace(/^\n\n/, '');
           resolve();
         } else {
           d2i.toSvg(node).then(dataUrl => {
-            svgs[i] = dataUrl.replace(/^[^<]*</, "<");
+            svgs[i] = dataUrl.replace(/^[^<]*</, '<');
             resolve();
           });
         }
@@ -78,27 +78,27 @@ export function exportSVG({ nodes, name }) {
   });
 }
 
-export function exportIndicatorsChart({ vizContainer, legend, name, mode }) {
+export function exportIndicatorsChart({vizContainer, legend, name, mode}) {
   // add legend to the viz container to include it in the snapshot
   const legendClone = legend.cloneNode(true);
   vizContainer.appendChild(legendClone);
 
   const options = {
-    style: { margin: 0, display: "flex", "flex-direction": "column" },
-    bgcolor: "#FFF",
+    style: {margin: 0, display: 'flex', 'flex-direction': 'column'},
+    bgcolor: '#FFF',
   };
-  if (mode === "PNG")
+  if (mode === 'PNG')
     return d2i.toBlob(vizContainer, options).then(blob => {
       // remove the legend
       vizContainer.removeChild(legendClone);
       return saveAs(blob, name);
     });
-  if (mode === "SVG")
+  if (mode === 'SVG')
     return d2i.toSvg(vizContainer, options).then(finalSvg => {
       // remove the legend
       vizContainer.removeChild(legendClone);
       return saveAs(
-        new Blob([finalSvg.replace("data:image/svg+xml;charset=utf-8,", "")], { type: "text/svg;charset=utf-8" }),
+        new Blob([finalSvg.replace('data:image/svg+xml;charset=utf-8,', '')], {type: 'text/svg;charset=utf-8'}),
         name,
       );
     });
